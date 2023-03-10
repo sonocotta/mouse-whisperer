0b5d3ea13d812f4c21e329085f6ee3441a079f1e
Andriy Malyshenko <andriy@malyshenko.com>
2023-03-10 14:37:01 +0100

    Stage prepare

--- firmware/attiny-mouse-whisperer/src/main.cpp
+++ firmware/attiny-mouse-whisperer/src/main.cpp
@@ -13,36 +13,7 @@ void setup()
   Serial.begin(115200);
   Serial.printf("Started");
 
-  // DigiMouse.begin();
-}
-
-ISR(PORTB_PORT_vect) {
-
-}
-
-void update_visual()
-{
-  #ifdef LIGHT_WS2812_AVR
-  if (enabled_state)
-  {
-    // uint8_t brightness = (abs(SIN(seq)) * MAX_BRT) >> 10;
-    // for (uint8_t i = 0; i < NUM_PIXELS; i++)
-    // {
-    //   leds[i] = (seq % NUM_PIXELS) == i ? (cRGB) {brightness, 0, 0} : RGB_COLOR_BLACK;
-    // }
-    ws2812_setleds(leds, NUM_PIXELS);
-    effect.loop();
-  }
-  else
-  {
-    for (uint8_t i = 0; i < NUM_PIXELS; i++)
-    {
-      leds[i] = (seq % NUM_PIXELS) == i ? (cRGB) {0, MAX_BRT >> 1, 0} : RGB_COLOR_BLACK;
-    }
-    ws2812_setleds(leds, NUM_PIXELS);
-  }
-  #endif
-  
+  DigiMouse.begin();
 }
 
 void loop()
